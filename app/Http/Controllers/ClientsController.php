ab8d01b2475b7b4aec72064080808b532287de8c
abdessalam staili <abdoycode@gmail.com>
2023-08-23 15:34:38 +0100

    todolist-api

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\clients;
+use App\Http\Requests\StoreclientsRequest;
+use App\Http\Requests\UpdateclientsRequest;
+
+class ClientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $clients = clients::all();
+        return ['clients' =>$clients];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreclientsRequest $request)
+    {
+        $client = clients::create($request->validated());
+        return ['client'=> $client];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateclientsRequest $request, clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(clients $clients)
+    {
+        //
+    }
+}
